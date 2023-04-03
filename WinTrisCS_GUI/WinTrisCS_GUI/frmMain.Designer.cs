b62e4b13ecb35992d15aece1d6aae3364f9ccac8
Riki <76902779+RikiCatte@users.noreply.github.com>
2023-04-03 17:02:56 +0200

    Add files via upload

---
+++ WinTrisCS_GUI/WinTrisCS_GUI/frmMain.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace WinTrisCS_GUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.pic9 = new System.Windows.Forms.PictureBox();
+            this.pic10 = new System.Windows.Forms.PictureBox();
+            this.pic11 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMatches = new System.Windows.Forms.TextBox();
+            this.txtVictories = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWinRate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pic12 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(12, 12);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(64, 64);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(82, 12);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(64, 64);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(152, 12);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(64, 64);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Location = new System.Drawing.Point(12, 82);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(64, 64);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4.TabIndex = 3;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic5
+            // 
+            this.pic5.Location = new System.Drawing.Point(82, 82);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(64, 64);
+            this.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic5.TabIndex = 4;
+            this.pic5.TabStop = false;
+            this.pic5.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic6
+            // 
+            this.pic6.Location = new System.Drawing.Point(152, 82);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(64, 64);
+            this.pic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic6.TabIndex = 5;
+            this.pic6.TabStop = false;
+            this.pic6.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic7
+            // 
+            this.pic7.Location = new System.Drawing.Point(12, 152);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(64, 64);
+            this.pic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic7.TabIndex = 6;
+            this.pic7.TabStop = false;
+            this.pic7.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic8
+            // 
+            this.pic8.Location = new System.Drawing.Point(82, 152);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(64, 64);
+            this.pic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic8.TabIndex = 7;
+            this.pic8.TabStop = false;
+            this.pic8.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic9
+            // 
+            this.pic9.Location = new System.Drawing.Point(152, 152);
+            this.pic9.Name = "pic9";
+            this.pic9.Size = new System.Drawing.Size(64, 64);
+            this.pic9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic9.TabIndex = 8;
+            this.pic9.TabStop = false;
+            this.pic9.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic10
+            // 
+            this.pic10.Image = ((System.Drawing.Image)(resources.GetObject("pic10.Image")));
+            this.pic10.Location = new System.Drawing.Point(12, 222);
+            this.pic10.Name = "pic10";
+            this.pic10.Size = new System.Drawing.Size(64, 64);
+            this.pic10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic10.TabIndex = 9;
+            this.pic10.TabStop = false;
+            this.pic10.Visible = false;
+            // 
+            // pic11
+            // 
+            this.pic11.Image = ((System.Drawing.Image)(resources.GetObject("pic11.Image")));
+            this.pic11.Location = new System.Drawing.Point(82, 222);
+            this.pic11.Name = "pic11";
+            this.pic11.Size = new System.Drawing.Size(64, 64);
+            this.pic11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic11.TabIndex = 10;
+            this.pic11.TabStop = false;
+            this.pic11.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Matches Count:";
+            // 
+            // txtMatches
+            // 
+            this.txtMatches.Location = new System.Drawing.Point(311, 10);
+            this.txtMatches.Name = "txtMatches";
+            this.txtMatches.Size = new System.Drawing.Size(35, 20);
+            this.txtMatches.TabIndex = 12;
+            // 
+            // txtVictories
+            // 
+            this.txtVictories.Location = new System.Drawing.Point(311, 34);
+            this.txtVictories.Name = "txtVictories";
+            this.txtVictories.Size = new System.Drawing.Size(35, 20);
+            this.txtVictories.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Victories Count:";
+            // 
+            // txtWinRate
+            // 
+            this.txtWinRate.Location = new System.Drawing.Point(311, 60);
+            this.txtWinRate.Name = "txtWinRate";
+            this.txtWinRate.Size = new System.Drawing.Size(35, 20);
+            this.txtWinRate.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(223, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Win Rate:";
+            // 
+            // pic12
+            // 
+            this.pic12.BackColor = System.Drawing.SystemColors.Control;
+            this.pic12.Image = ((System.Drawing.Image)(resources.GetObject("pic12.Image")));
+            this.pic12.Location = new System.Drawing.Point(152, 222);
+            this.pic12.Name = "pic12";
+            this.pic12.Size = new System.Drawing.Size(64, 64);
+            this.pic12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic12.TabIndex = 17;
+            this.pic12.TabStop = false;
+            this.pic12.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 220);
+            this.Controls.Add(this.pic12);
+            this.Controls.Add(this.txtWinRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtVictories);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMatches);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic11);
+            this.Controls.Add(this.pic10);
+            this.Controls.Add(this.pic9);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic7);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.PictureBox pic9;
+        private System.Windows.Forms.PictureBox pic10;
+        private System.Windows.Forms.PictureBox pic11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMatches;
+        private System.Windows.Forms.TextBox txtVictories;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWinRate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pic12;
+    }
+}
+
---
+++ WinTrisCS_GUI/WinTrisCS_GUI/frmMain.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinTrisCS_GUI
+{
+    public partial class frmMain : Form
+    {
+        //TAG=0 PIC EMPTY, TAG=1 PLAYER1, TAG=2 PLAYER2
+
+        uint matchesCount;
+        int victoriesCount; // Incremented if there is a victory, decrement if not
+        uint winRate; // wins / defeats %
+        Random r = new Random();
+        int picChoosen;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Text = "WinTris by RikiCatte#1371";
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            StartPosition = FormStartPosition.CenterScreen;
+            txtMatches.ReadOnly = true;
+            txtVictories.ReadOnly = true;
+            txtWinRate.ReadOnly = true;
+            matchesCount = 0;
+            victoriesCount = 0;
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initializes the game field, MUST be called at the beginning of every match
+        /// </summary>
+        private void Initialize()
+        {
+            pic1.Image = pic12.Image;
+            pic2.Image = pic12.Image;
+            pic3.Image = pic12.Image;
+            pic4.Image = pic12.Image;
+            pic5.Image = pic12.Image;
+            pic6.Image = pic12.Image;
+            pic7.Image = pic12.Image;
+            pic8.Image = pic12.Image;
+            pic9.Image = pic12.Image;
+            pic1.Tag = "0";
+            pic2.Tag = "0";
+            pic3.Tag = "0";
+            pic4.Tag = "0";
+            pic5.Tag = "0";
+            pic6.Tag = "0";
+            pic7.Tag = "0";
+            pic8.Tag = "0";
+            pic9.Tag = "0";
+        }
+
+        /// <summary>
+        /// Must be called when a player finishes his turn, in this way the enemy cannot overwrite an already occupied field
+        /// </summary>
+        private void SwitchPlayer()
+        {
+            pic1.Enabled = !pic1.Enabled;
+            pic2.Enabled = !pic2.Enabled;
+            pic3.Enabled = !pic3.Enabled;
+            pic4.Enabled = !pic4.Enabled;
+            pic5.Enabled = !pic5.Enabled;
+            pic6.Enabled = !pic6.Enabled;
+            pic7.Enabled = !pic7.Enabled;
+            pic8.Enabled = !pic8.Enabled;
+            pic9.Enabled = !pic9.Enabled;
+        }
+
+        /// <summary>
+        /// Updates the score in the relatives gaps in frmMain
+        /// </summary>
+        private void UpdateScore()
+        {
+            txtMatches.Text = matchesCount.ToString();
+            txtVictories.Text = victoriesCount.ToString();
+            winRate = (uint)Math.Round(100 * ((double)victoriesCount / matchesCount));
+            txtWinRate.Text = winRate.ToString() + "%";
+        }
+
+        /// <summary>
+        /// Check if someone has won, MUST be called at every turn
+        /// </summary>
+        /// <returns></returns>
+        private bool VictoryCheck()
+        {
+            // First row check
+            if ((pic1.Tag != "0") && (pic1.Tag == pic2.Tag) && (pic1.Tag == pic3.Tag))
+                return true;
+            // Second row check
+            else if ((pic4.Tag != "0") && (pic4.Tag == pic5.Tag) && (pic4.Tag == pic6.Tag))
+                return true;
+            // Third row check
+            else if ((pic7.Tag != "0") && (pic7.Tag == pic8.Tag) && (pic7.Tag == pic9.Tag))
+                return true;
+            // First column check
+            else if ((pic1.Tag != "0") && (pic1.Tag == pic4.Tag) && (pic1.Tag == pic7.Tag))
+                return true;
+            // Second column check
+            else if ((pic2.Tag != "0") && (pic2.Tag == pic5.Tag) && (pic2.Tag == pic8.Tag))
+                return true;
+            // Third column check
+            else if ((pic3.Tag != "0") && (pic3.Tag == pic6.Tag) && (pic3.Tag == pic9.Tag))
+                return true;
+            // Principal diagonal check
+            else if ((pic1.Tag != "0") && (pic1.Tag == pic5.Tag) && (pic1.Tag == pic9.Tag))
+                return true;
+            // Secondary diagonal check 
+            else if ((pic3.Tag != "0") && (pic3.Tag == pic5.Tag) && (pic3.Tag == pic7.Tag))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Check if all fields are occupied, MUST be called EVER
+        /// </summary>
+        /// <returns>true if the game field is complete, false if not</returns>
+        private bool IsGameFinished()
+        {
+            if (pic1.Tag != "0" && pic2.Tag != "0" && pic3.Tag != "0" && pic4.Tag != "0" && pic5.Tag != "0" && pic6.Tag != "0" && pic7.Tag != "0" && pic8.Tag != "0" && pic9.Tag != "0")
+                return true; // Game finished
+            else
+                return false; // Game not finished
+        }
+
+        /// <summary>
+        /// Used in Bot() method
+        /// </summary>
+        private int ChoosePictureBox()
+        {
+            return r.Next(1, 10); // Generates a number between 1 and 9 to random choose a picbox
+        }
+
+        /// <summary>
+        /// Pc's turn here
+        /// </summary>
+        private void Bot() // MUST BE DONE BETTER
+        {
+        rnd:
+            picChoosen = ChoosePictureBox();
+
+            if (picChoosen == 1 && pic1.Tag == "0")
+            {
+                pic1.Tag = "2"; // Marks the PictureBox as occupied by the pc
+                pic1.Image = pic11.Image; // Loads the X image
+            }
+            else if (picChoosen == 2 && pic2.Tag == "0")
+            {
+                pic2.Tag = "2";
+                pic2.Image = pic11.Image;
+            }
+            else if (picChoosen == 3 && pic3.Tag == "0")
+            {
+                pic3.Tag = "2";
+                pic3.Image = pic11.Image;
+            }
+            else if (picChoosen == 4 && pic4.Tag == "0")
+            {
+                pic4.Tag = "2";
+                pic4.Image = pic11.Image;
+            }
+            else if (picChoosen == 5 && pic5.Tag == "0")
+            {
+                pic5.Tag = "2";
+                pic5.Image = pic11.Image;
+            }
+            else if (picChoosen == 6 && pic6.Tag == "0")
+            {
+                pic6.Tag = "2";
+                pic6.Image = pic11.Image;
+            }
+            else if (picChoosen == 7 && pic7.Tag == "0")
+            {
+                pic7.Tag = "2";
+                pic7.Image = pic11.Image;
+            }
+            else if (picChoosen == 8 && pic8.Tag == "0")
+            {
+                pic8.Tag = "2";
+                pic8.Image = pic11.Image;
+            }
+            else if (picChoosen == 9 && pic9.Tag == "0")
+            {
+                pic9.Tag = "2";
+                pic9.Image = pic11.Image;
+            }
+            else
+                goto rnd; // Need another number because the field is already occupied
+
+            if (VictoryCheck())
+            {
+                matchesCount++;
+                victoriesCount--;
+                UpdateScore();
+
+                if (MessageBox.Show("You lose!\n\nDo you want to play again?", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) // Game ended
+                    Close();
+                else
+                    Initialize();
+            }
+            else if (IsGameFinished())
+            {
+                if (MessageBox.Show("No one won!\n\nDo you want to play again?", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) // Game ended
+                    Close();
+                else
+                    Initialize();
+            }
+            else
+                SwitchPlayer(); // Game not ended
+        }
+
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;
+            pic.Image = pic10.Image;
+            pic.Tag = "1"; // Marks the PictureBox as occupied by the player
+
+            if (VictoryCheck())
+            {
+                matchesCount++;
+                victoriesCount++;
+                UpdateScore();
+
+                if (MessageBox.Show("You won!\n\nDo you want to play again?", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) // Game ended
+                    Close();
+                else
+                    Initialize();
+
+                return;
+            }
+            else if(IsGameFinished())
+            {
+                matchesCount++;
+                UpdateScore();
+
+                if (MessageBox.Show("No one won!\n\nDo you want to play again?", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) // Game ended
+                    Close();
+                else
+                    Initialize();
+            }
+            else
+            {
+                SwitchPlayer(); // Changes the turn into bot's turn
+                Bot();
+            }
+        }
+    }
+}
